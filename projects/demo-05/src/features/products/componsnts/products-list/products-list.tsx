34a3497ad9e01cf74bdc2ddb055bdc57c0b8da42
Alejandro Cerezo <alce65@hotmail.es>
2026-06-04 20:23:21 +0200

    Refactor products-list with a custom hook

--- projects/demo-05/src/features/products/componsnts/products-list/products-list.tsx
+++ projects/demo-05/src/features/products/componsnts/products-list/products-list.tsx
@@ -1,40 +1,10 @@
-import { useEffect, useState } from 'react';
-import type { Product, ProductCreateDTO } from '../../entities/products';
-import { productsRepo } from '../../services/products-repo';
 import './products-list.css';
 import { ProductItem } from '../product-item/product-item';
 import { ProductForm } from '../product-form/product-form';
+import { useProducts } from './use-products';
 
 export const ProductsList: React.FC = () => {
-    const [products, setProducts] = useState<Product[]>([]);
-
-    useEffect(() => {
-        const products = productsRepo.getProducts();
-        // eslint-disable-next-line react-hooks/set-state-in-effect
-        setProducts(products);
-    }, []);
-
-
-    const editProduct = (product: Product) => {
-            console.log(`Producto con id ${product.id} preparado para edición`);
-            // Open ProductForm with product data for editing
-            setProducts((prevProducts) =>
-                prevProducts.map((p) => (p.id === product.id ? product : p)),
-            );
-    }
-
-    const addProduct = (product: ProductCreateDTO) => {
-        const newProduct: Product = {
-            id: crypto.randomUUID().slice(0, 4),
-            ...product
-        };
-        setProducts([newProduct, ...products]);
-    };
-
-    const deleteProduct = (id: string) => {
-        const updatedProducts = products.filter((product) => product.id !== id);
-        setProducts(updatedProducts);
-    };
+    const { products, editProduct, addProduct, deleteProduct } = useProducts();
 
     return (
         <>
